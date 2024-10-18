7d9a7113274a51ff430d3688c076b64b8bdb17da
whdgur2498 <whdgur2498@gmail.com>
2024-10-18 12:31:45 +0900

    [Add]Rocket 연료소모
    연료가 다 떨어진 상태에서는 AddForce 작동안하게

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -1,9 +1,11 @@
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Rocket : MonoBehaviour
 {
     private Rigidbody2D _rb2d;
+    private Button _button;
     private float fuel = 100f;
     
     private readonly float SPEED = 5f;
@@ -13,12 +15,18 @@ public class Rocket : MonoBehaviour
     {
         // TODO : Rigidbody2D 컴포넌트를 가져옴(캐싱)
         _rb2d = GetComponent<Rigidbody2D>();
+        _button = GetComponent<Button>();
     }
-    
+
     public void Shoot()
     {
-        // TODO : fuel이 넉넉하면 윗 방향으로 SPEED만큼의 힘으로 점프, 모자라면 무시
-        _rb2d.AddForce(Vector2.up * SPEED, ForceMode2D.Impulse);
-        // 버튼 한번당 -10 바닥에 닿으면 초기화
+        if (fuel > 0) 
+        {
+            Debug.Log("fire");
+            // TODO : fuel이 넉넉하면 윗 방향으로 SPEED만큼의 힘으로 점프, 모자라면 무시
+            _rb2d.AddForce(Vector2.up * SPEED, ForceMode2D.Impulse);
+            // 버튼 한번당 -10 바닥에 닿으면 초기화
+            fuel -= FUELPERSHOOT;
+        }
     }
 }
